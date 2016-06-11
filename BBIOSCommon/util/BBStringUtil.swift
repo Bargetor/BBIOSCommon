bd8908d5afe5d81071509a5372d5d23641c84aaf
Bargetor <bargetor@gmail.com>
2016-06-11 20:51:31 +0800

    优化代码

--- BBIOSCommon/util/BBStringUtil.swift
+++ BBIOSCommon/util/BBStringUtil.swift
@@ -10,15 +10,19 @@ import Foundation
 
 extension String {
     
-    func substringWithRange(range: NSRange) -> String! {
+    public func substringWithRange(range: NSRange) -> String! {
         let r = Range<String.Index>(start: self.startIndex.advancedBy(range.location), end: self.startIndex.advancedBy(range.location + range.length))
         return self.substringWithRange(r)
     }
         
-    func urlencode() -> String {
+    public func urlencode() -> String {
         let urlEncoded = self.stringByReplacingOccurrencesOfString(" ", withString: "+")
         let chartset = NSMutableCharacterSet(bitmapRepresentation: NSCharacterSet.URLQueryAllowedCharacterSet().bitmapRepresentation)
         chartset.removeCharactersInString("!*'();:@&=$,/?%#[]")
         return urlEncoded.stringByAddingPercentEncodingWithAllowedCharacters(chartset)!
+    }
+    
+    public func specialCharsProcess() -> String{
+        return self.stringByReplacingOccurrencesOfString("&nbsp;", withString: "", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
     }
 }
