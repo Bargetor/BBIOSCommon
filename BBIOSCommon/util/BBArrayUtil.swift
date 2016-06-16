65cc2f3a0398f344cfc089a3a162383c480ea784
Bargetor <bargetor@gmail.com>
2016-06-17 01:12:12 +0800

    优化代码

--- BBIOSCommon/util/BBArrayUtil.swift
+++ BBIOSCommon/util/BBArrayUtil.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension Array{
     
-    func subArrayWithRange(location: Int, length: Int) -> Array<Element>?{
+    public func subArrayWithRange(location: Int, length: Int) -> Array<Element>?{
         let count = self.count
         if(location >= count){
             return nil
@@ -24,6 +24,41 @@ extension Array{
         }
         
         return result
-
+        
     }
+    
+    public func toUnsafeMutablePointer() -> UnsafeMutablePointer<Element>{
+        let points: UnsafeMutablePointer<Element> = UnsafeMutablePointer.alloc(self.count)
+        for i in 0 ..< self.count{
+            let item = self[i]
+            points[i] = item
+        }
+        
+        return points
+    }
+    
+    public mutating func appendAll(elements: [Element]?){
+        guard let elements = elements else{
+            return
+        }
+        
+        for element in elements{
+            self.append(element)
+        }
+    }
+}
+
+extension Array where Element: Equatable {
+    
+    /**
+     从array中删除一个对象
+     
+     - parameter object: 要删除的对象
+     */
+    public mutating func removeObject(object: Element) {
+        if let index = self.indexOf(object) {
+            self.removeAtIndex(index)
+        }
+    }
+    
 }
